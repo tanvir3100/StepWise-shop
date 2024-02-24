ddfc59924ecccaba58dce4c84d993e6610338992
tanvir <tanvir0046@gmail.com>
2024-02-24 20:54:34 +0600

    hero section setup is ready

--- src/Components/Share/Navbar.jsx
+++ src/Components/Share/Navbar.jsx
@@ -36,16 +36,18 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl font-bold">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52 font-bold gap-4 text-black">
                             {navLinks}
                         </ul>
                     </div>
-                    <ul className="menu menu-horizontal px-1 gap-4 text-lg font-semibold">
-                        {navLinks}
-                    </ul>
+                    <div className="hidden lg:block w-full">
+                        <ul className="menu menu-horizontal px-1 gap-5 text-lg font-semibold">
+                            {navLinks}
+                        </ul>
+                    </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <a className="btn btn-ghost text-xl">STEPWISE</a>
+                <div className="navbar-center hidden lg:flex w-40">
+                    <img className="w-full" src="https://i.ibb.co/pRW1vtY/STEPWISE-removebg-preview-removebg-preview.png" alt="" />
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
